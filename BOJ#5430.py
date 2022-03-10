01af58f7789ef4e463f740c7665b45a6aac74db6
valofosho <whgdk1880@naver.com>
2022-03-10 17:30:16 +0900

    03/10

--- BOJ#5430.py
+++ BOJ#5430.py
@@ -23,8 +23,10 @@ for i in range(T):
             else:
                 arr=Deletion(arr)
     print(arr)"""
-
+"""
 import ast
+import sys
+input=sys.stdin.readline
 def binding(arr,flow):
     for i in range(len(flow)):
         if len(arr) !=0:
@@ -43,4 +45,27 @@ for i in range(T):
     arr=input()
     arr=ast.literal_eval(arr)
     print(binding(arr,flow))
+"""
 
+def deque_R(arr,Rn):
+    if Rn%2 == 0:
+        return arr
+    else:
+        arr.reverse()
+        return arr
+
+def deque_D(arr):
+    if arr:
+        return arr.popleft()
+import sys
+from collections import deque
+import ast
+for i in range(T):
+    flow=str(input())
+    n=input()
+    arr=input()
+    arr=ast.literal_eval(arr)
+    a=deque(arr)
+    print(binding(arr,flow))
+
+print(a)
