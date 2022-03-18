4788f51aee61350be4f5fb39fc697f98a875b0e3
valofosho <whgdk1880@naver.com>
2022-03-18 17:46:33 +0900

    03/18

--- BOJ#5430.py
+++ BOJ#5430.py
@@ -70,6 +70,7 @@ for i in range(T):
 
 print(a)
 """
+"""
 import ast
 import sys
 from _collections import deque
@@ -94,3 +95,60 @@ for i in range(T):
     print(arr)
     a = deque(arr)
     print(binding(arr,flow))
+"""
+
+
+
+
+
+import sys
+import ast
+from collections import deque
+input=sys.stdin.readline
+Tc=int(input())
+for i in range (Tc):
+    order=str(input())
+    num=int(input())
+    str_arr=input()
+    arr=ast.literal_eval(str_arr)
+    de=deque(arr)
+    Rcount = 0
+    for j in range(len(order)-1):
+        if j == (len(order)-2):
+            if order[j] == "R":
+                Rcount += 1
+                if Rcount % 2 == 0:
+                    de=str(de)[6:-1].replace(', ',',')
+                    print(de)
+                    break
+                elif Rcount % 2 !=0:
+                    de.reverse()
+                    de = str(de)[6:-1].replace(', ', ',')
+                    print(de)
+                    break
+            elif order[j] == "D":
+                if len(de) == 0:
+                    print("error")
+                    break
+                elif Rcount % 2 == 0:
+                    de.popleft()
+                    de=str(de)[6:-1].replace(', ',',')
+                    print(de)
+                    break
+                elif Rcount % 2 != 0:
+                    de.pop()
+                    de.reverse()
+                    print(str(de)[6:-1].replace(', ',','))
+                    break
+
+        else:
+            if order[j] == "R":
+                Rcount += 1
+            elif order[j] == "D":
+                if len(de) == 0:
+                    print("error")
+                    break
+                elif Rcount % 2 == 0:
+                    de.popleft()
+                elif Rcount % 2 != 0:
+                    de.pop()
