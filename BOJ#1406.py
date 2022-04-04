4186542206ccf370375c8751a01b7c5aa17e7128
valofosho <whgdk1880@naver.com>
2022-04-04 17:56:09 +0900

    04/04

--- BOJ#1406.py
+++ BOJ#1406.py
@@ -40,3 +40,21 @@ for i in range(cnt):
     print(ls,cursor)
 print(ls)"""
 
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class SingleLinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.count = 0
+
+    def append(self, node):
+        if self.head == None:
+            self.head = node
+        else:
+            cur = self.head
+            while cur.next != None:
+                cur = cur. next
+            cur.next = node
+    
