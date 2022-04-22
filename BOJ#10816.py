fa6c798c1701fb7b72f0cffa685e4098de73609d
valofosho <whgdk1880@naver.com>
2022-04-22 17:47:53 +0900

    04/22

--- BOJ#10816.py
+++ BOJ#10816.py
@@ -1,23 +1,25 @@
-def check(arr, target, start, end, cnt):
+def sol(arr, start, end, target, say):
     if start > end:
-        return 0
+        say.append(0)
+        return
     mid = (start + end) // 2
-    if arr[mid] == target:
-        cnt += 1
-
-
+    if arr[mid][0] > target:
+        return sol(arr, start, mid-1, target, say)
+    elif arr[mid][0] < target:
+        return sol(arr, mid+1, end, target, say)
+    elif arr[mid][0] == target:
+        say.append(arr[mid][1])
+        return
 
 import sys
+from collections import Counter
 input = sys.stdin.readline
 N = int(input().rstrip())
-ls = list(map(int,input().split()))
-ls.sort()
+ls = sorted(Counter(list(map(int,input().split()))).items(), key = lambda x : x[0])
 M = int(input().rstrip())
 lss = list(map(int,input().split()))
-num = []
-cnt = 0
+says=[]
+lenN=len(ls)
 for i in range(M):
-    cnt = ls.count(lss[i])
-    num.append(cnt)
-    cnt = 0
-print(" ".join(map(str, num)))
+    sol(ls, 0, lenN-1,lss[i],says)
+print(" ".join(map(str,says)))
