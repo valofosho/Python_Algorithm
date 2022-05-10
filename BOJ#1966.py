48201ed796e42772387daf2c67886b420f5b1cd0
valofosho <whgdk1880@naver.com>
2022-05-10 17:39:07 +0900

    05/10

--- BOJ#1966.py
+++ BOJ#1966.py
@@ -1,5 +1,3 @@
-
-
 from collections import deque
 import sys
 input = sys.stdin.readline
@@ -7,9 +5,43 @@ N = int(input())
 for i in range(N):
     numP, place = map(int,input().split())
     imp = deque(list(map(int,input().split())))
+    order = deque([])
+    for j in range(numP):
+        while j != place:
+            order.append(0)
+        order.append('a')
     maximum = max(imp)
-    cnt = 1
-    while imp[0] != max(imp):
-        imp.rotate(-1)
-        cnt += 1
-        if imp[place]
+    cnt = 0
+    stop = 0
+    while stop != 1 :
+        if imp[0] == maximum:
+            if order[0] != 'a':
+                imp.popleft()
+                maximum = max(imp)
+                cnt += 1
+            else:
+                print(cnt)
+                stop += 1
+        else:
+            imp.rotate(-1)
+            order.rotate(-1)
+            cnt += 1
+
+
+"""
+문제 예시 
+3
+1 0
+5
+4 2
+1 2 3 4
+6 0 
+1 1 9 1 1 1
+
+중요 포인트
+! 중요도는 1이상 9이하 정수
+! 중요도는 겹칠 수 있다.
+
+해결방안
+! 
+"""
